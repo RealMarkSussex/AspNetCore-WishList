d2daf9d0be62ea7b1eafaa520a776976ea6b81b6
RealMarkSussex <mark.sussex@gocompare.com>
2020-02-13 15:47:30 +0000

    module 4

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
