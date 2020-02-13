b430871c8e4c016fc9000cba9bd4135708671453
RealMarkSussex <mark.sussex@gocompare.com>
2020-02-13 16:11:01 +0000

    Create ItemController.cs

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+        }
+        public IActionResult Index()
+        {
+            return View("Index", _context.Items.ToList());
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        public IActionResult Create(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Create");
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            _context.Items.Find(Id);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
